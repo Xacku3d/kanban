7b643fae1860432c865fcf14e7d2b2abeba98257
Xacku3d <41637751+Xacku3d@users.noreply.github.com>
2019-07-04 16:21:20 +0300

    Updates

---
+++ precache-manifest.c6148fecd9b361d5a76e38bcb02f6362.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2610cd45f1eddd119f6c710ecb8c44e",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "6204f703441012ba9f28",
+    "url": "/kanban/static/js/2.cb996585.chunk.js"
+  },
+  {
+    "revision": "d547f646eaa9567ae609",
+    "url": "/kanban/static/js/main.5e0458ff.chunk.js"
+  },
+  {
+    "revision": "9c81d15565ccb12a85c0",
+    "url": "/kanban/static/js/runtime~main.4f25e9d8.js"
+  }
+]);
